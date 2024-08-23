315433f19824d9c49cc33170b5c85fb33af7a334
jespy2 <jespy2@gmail.com>
2024-08-23 08:34:45 -0600

    updated search logic to be more reactive to user input

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,11 @@ import { SearchBar, UserCard } from "./components/";
 
 export default function App() {
 	const [users, setUsers] = useState<User[] | null>(null);
+	// Note:  This is a brute force approach to managing this state due to a tight timeline and the nature of the assessment.  IRL, I'd want to look at having the formatUser functionality mutate User objects on the backend, or maybe as part of middleware/reducers within a more comprehensive state management strategy so that users are fetched => reducers apply formatUser => formattedUsers are injected into the central source of truth (ex. store for redux).  Either way, having this many interdependent state updates complicates the state architecture and would make debugging and adding functionality more problematic.
 	const [formattedUsers, setFormattedUsers] = useState<User[] | null>(null);
+	const [displayedUsers, setDisplayedUsers] = useState<User[] | null>(null);
 	const [selectedUser, setSelectedUser] = useState<string | null>(null);
+	const [searchQuery, setSearchQuery] = useState<string>("");
 
 	// useEffects are a good cue that a custom hook may be more appropriate here.  Would consider the needs of the larger project to determine if this could/should be generified for use throughout the codebase, thus requiring conversion to a custom hook.  Leaving as-is per my understanding of the assessment specs
 	useEffect(() => {
@@ -23,36 +26,49 @@ export default function App() {
 
 	useEffect(() => {
 		const _formattedUsers = users?.map((user) => formatUser(user));
-		if (_formattedUsers) setFormattedUsers(_formattedUsers);
+		if (_formattedUsers) {
+			setFormattedUsers(_formattedUsers);
+			setDisplayedUsers(_formattedUsers);
+		}
 	}, [users]);
 
 	const userClicked = (id: string) => {
 		setSelectedUser(id);
-  };
-  
-  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchValue = e.target.value.toLowerCase();
-    const filteredUsers = formattedUsers?.filter(user => {
-      return user.name.toLowerCase().includes(searchValue) || user.id.toLowerCase().includes(searchValue) || user.friends.some(friendId => friendId.toLowerCase().includes(searchValue))
-    });
-    filteredUsers && setFormattedUsers(filteredUsers);
-  }
+	};
+
+	// 7. Implement searching functionality by name, ID, or friend's ID. Allow users to search for users by entering a query in an input field. Display only the users that match the search criteria. The search should be case-insensitive and match any part of the user's name, ID, or friend's ID.
+	useEffect(() => {
+		const filteredUsers = formattedUsers?.filter((user) => {
+			return (
+				user.name.toLowerCase().includes(searchQuery) ||
+				user.id.toLowerCase().includes(searchQuery) ||
+				user.friends.some((friendId) =>
+					friendId.toLowerCase().includes(searchQuery)
+				)
+			);
+		});
+		filteredUsers && setDisplayedUsers(filteredUsers);
+	}, [searchQuery]);
+
+	const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchQuery(e.target.value.toLowerCase());
+	};
 
 	// 6. Implement memoization for the User components to optimize performance. The components should only re-render when the user data is updated.
 	const userList = useMemo(() => {
-		return formattedUsers?.map((user) => (
+		return displayedUsers?.map((user) => (
 			<UserCard
 				user={user}
 				onClick={userClicked}
 				isSelected={user.id === selectedUser}
 			/>
 		));
-	}, [formattedUsers, selectedUser]);
+	}, [displayedUsers, selectedUser]);
 
 	return (
-		<div className="app-container">
+		<div className='app-container'>
 			<SearchBar onChange={onSearchChange} />
 			{formattedUsers && <div className='user-list'>{userList}</div>}
-		</ div>
+		</div>
 	);
 }
